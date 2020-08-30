44024b2483b0de6dd7f3eb345216f6450790c1cf
condor2 <condor20_05@yahoo.it>
2020-08-30 13:06:29 +0300

    Version 2.3.0.2 updated

---
+++ v.2.3.x/upload/admin/language/ro-ro/extension/payment/mobilpay.php
@@ -0,0 +1,32 @@
+<?php
+// Heading
+$_['heading_title']							= 'Plata cu cardul - MobilPay';
+
+// Text 
+$_['text_extension']						= 'Extensii';
+$_['text_success']							= 'Succes: Ai modificat MobilPay!';
+$_['text_edit']								= 'Editează MobilPay';
+$_['text_test']								= 'Test';
+$_['text_live']								= 'Live';
+$_['text_mobilpay']							= '<a target="_blank" href="https://netopia-payments.com/"><img src="view/image/payment/mobilpay.gif" alt="mobilPay Romania" title="mobilPay Romania" style="border: 1px solid #EEEEEE;" /></a>';
+
+// Entry
+$_['entry_signature']						= 'Cheie unică cont comerciant';
+$_['entry_test']							= 'Modul sandbox';
+$_['entry_order_status']					= 'Status "Confirmată"';
+$_['entry_order_status_confirmed_pending']	= 'Status "Confirmare în așteptare"';
+$_['entry_order_status_paid_pending']		= 'Status "Plată în așteptare"';
+$_['entry_order_status_paid']				= 'Status "Plătită';
+$_['entry_order_status_canceled']			= 'Status "Anulată"';
+$_['entry_order_status_credit']				= 'Status "Credit"';
+$_['entry_geo_zone']						= 'Zona Geografică';
+$_['entry_status']							= 'Status';
+$_['entry_sort_order']						= 'Ordine Sortare';
+$_['entry_total']							= 'Total';
+
+// Help
+$_['help_total']							= 'Totalul pe care comanda trebuie să-l atingă înainte ca această metodă de plată să devină activă.';
+
+// Error
+$_['error_permission']						= 'Eroare: Nu ai permisiunile necesare pentru a modifica mobilPay!';
+$_['error_signature']						= 'Cheia Unică este necesară!';
